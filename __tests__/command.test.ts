38b28ed79ce5700b46edeb5c0eaa29a4f9212179
Technote <technote.space@gmail.com>
2020-09-25 22:31:58 +0900

    feat: add addPath mock helper (#144)

--- __tests__/command.test.ts
+++ __tests__/command.test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-magic-numbers */
-import {spyOnExportVariable, exportVariableCalledWith} from '../src';
-import {exportVariable} from '@actions/core';
+import {spyOnExportVariable, exportVariableCalledWith, spyOnAddPath, addPathCalledWith} from '../src';
+import {addPath, exportVariable} from '@actions/core';
 
 describe('spyOnExportVariable, exportVariableCalledWith', () => {
   it('should spy on exportVariable', () => {
@@ -13,5 +13,16 @@ describe('spyOnExportVariable, exportVariableCalledWith', () => {
       {name: 'test-name1', val: 'test-value1'},
       {name: 'test-name2', val: ['test-value2-1', 'test-value2-2']},
     ]);
+  });
+});
+
+describe('spyOnAddPath, addPathCalledWith', () => {
+  it('should spy on exportVariable', () => {
+    const spy = spyOnAddPath();
+
+    addPath('test-path1');
+    addPath('test-path2');
+
+    addPathCalledWith(spy, ['test-path1', 'test-path2']);
   });
 });
