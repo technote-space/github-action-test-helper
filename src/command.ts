d43c115c2b3cf470ecab2ed97e95eb042364eb40
Technote <technote.space@gmail.com>
2020-09-25 22:03:52 +0900

    feat: add exportVariable mock helper (#143)

---
+++ src/command.ts
@@ -0,0 +1,12 @@
+import * as core from '@actions/core' ;
+import SpyInstance = jest.SpyInstance;
+
+export const spyOnExportVariable      = (): SpyInstance => jest.spyOn(core, 'exportVariable').mockReturnValue();
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const exportVariableCalledWith = (spyOnMock: SpyInstance, pairs: { name: string, val: any }[]): void => {
+  expect(spyOnMock).toBeCalledTimes(pairs.length);
+  pairs.forEach(({name, val}, index) => {
+    expect(spyOnMock.mock.calls[index][0]).toBe(name);
+    expect(spyOnMock.mock.calls[index][1]).toBe(val);
+  });
+};
