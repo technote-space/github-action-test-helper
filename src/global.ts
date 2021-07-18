fa4659237a63b249e769ef5a19bf883f403422bd
Technote <technote.space@gmail.com>
2021-07-18 22:08:42 +0900

    fix: type error

--- src/global.ts
+++ src/global.ts
@@ -1,17 +1,19 @@
 import {ExecException} from 'child_process';
 
-interface Global extends NodeJS.Global {
-  mockStdout: {
-    write: jest.Mock;
-  };
-  mockChildProcess: {
-    exec: jest.Mock;
-    spawn: jest.Mock;
-    stdout: string | ((command: string) => string);
-    stderr: string | ((command: string) => string);
-    error: ExecException | null | ((command: string) => ExecException | null);
-    code: number | ((command: string) => number);
-  };
+declare global {
+  interface Global {
+    mockStdout: {
+      write: jest.Mock;
+    };
+    mockChildProcess: {
+      exec: jest.Mock;
+      spawn: jest.Mock;
+      stdout: string | ((command: string) => string);
+      stderr: string | ((command: string) => string);
+      error: ExecException | null | ((command: string) => ExecException | null);
+      code: number | ((command: string) => number);
+    };
+  }
 }
 
 declare const global: Global;
