f702c4b249c1e947a3689f78b177b48779cbdf91
Technote <technote.space@gmail.com>
2022-04-20 01:26:43 +0900

    feat: esm

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import pluginTypescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/index.mjs',
+    format: 'es',
+  },
+  external: ['vitest'],
+  plugins: [
+    pluginTypescript(),
+  ],
+};
